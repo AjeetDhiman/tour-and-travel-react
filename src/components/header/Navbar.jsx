0388e74578026b9453897973ef8049c54c2c9289
Ajeet Dhiman <ak8445706236@gmail.com>
2025-05-26 14:44:04 +0530

    animation removed framer motion

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -2,12 +2,10 @@ import { useState, useEffect } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
 import { Menu, X } from "lucide-react";
-import { motion, AnimatePresence } from "framer-motion";
 import clsx from "clsx";
 import Logo from "./Logo";
 import Button from "../ui/Button";
 import Container from "../misc/Container";
-import { navItemVariant } from "../misc/transition.js";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -79,91 +77,63 @@ const Navbar = () => {
       <nav>
         <Container>
           <div className="flex flex-row items-center justify-between py-4 xl:py-8">
-            <motion.div
-              initial={{ opacity: 0, translateY: 20 }}
-              animate={{ opacity: 1, translateY: 0 }}
-              transition={{
-                duration: 0.4,
-                delay: 0.1,
-                ease: "easeOut",
-              }}
-            >
+            <div>
               <Logo />
-            </motion.div>
-            <AnimatePresence>
-              {!isDesktop && isOpen && (
-                <motion.div
-                  initial={{ opacity: 0, x: "-100%" }}
-                  animate={{ opacity: 1, x: "0" }}
-                  exit={{ opacity: 0, x: "-100%" }}
-                  transition={{ duration: 0.4 }}
-                  className="fixed inset-0 z-40 bg-black/40 backdrop-blur-sm lg:hidden"
+            </div>
+            {!isDesktop && isOpen && (
+              <div
+                className="fixed inset-0 z-40 bg-black/40 backdrop-blur-sm lg:hidden"
+                onClick={() => setIsOpen(false)}
+              />
+            )}
+            <div
+              className={`${
+                isDesktop ? "relative flex-row" : "fixed top-0 left-0 z-50"
+              } min-h-screen min-w-3xs bg-white px-4 pt-12 pb-4 shadow lg:min-h-auto lg:w-auto lg:bg-transparent lg:p-0 lg:shadow-none`}
+            >
+              {!isDesktop && (
+                <Button
+                  className="absolute top-4 right-4 cursor-pointer"
                   onClick={() => setIsOpen(false)}
-                />
-              )}
-            </AnimatePresence>
-            <AnimatePresence>
-              <motion.div
-                initial={isDesktop ? { opacity: 0 } : { x: "-100%" }}
-                animate={
-                  isDesktop ? { opacity: 1 } : { x: isOpen ? 0 : "-100%" }
-                }
-                exit={isDesktop ? { opacity: 0 } : { x: "-100%" }}
-                transition={{ duration: 0.3, ease: "easeInOut" }}
-                className={`${
-                  isDesktop ? "relative flex-row" : "fixed top-0 left-0 z-50"
-                } min-h-screen min-w-3xs bg-white px-4 pt-12 pb-4 shadow lg:min-h-auto lg:w-auto lg:bg-transparent lg:p-0 lg:shadow-none`}
-              >
-                {!isDesktop && (
-                  <Button
-                    className="absolute top-4 right-4 cursor-pointer"
-                    onClick={() => setIsOpen(false)}
-                  >
-                    <X size={24} />
-                  </Button>
-                )}
-                <div
-                  className={`flex flex-col gap-y-4 lg:flex-row lg:gap-x-3 xl:gap-x-[40px]`}
                 >
-                  {pageURL.map((link, index) => {
-                    return (
-                      <motion.div
+                  <X size={24} />
+                </Button>
+              )}
+              <div
+                className={`flex flex-col gap-y-4 lg:flex-row lg:gap-x-3 xl:gap-x-[40px]`}
+              >
+                {pageURL.map((link, index) => {
+                  return (
+                    <div>
+                      <NavLink
                         key={index}
-                        custom={index}
-                        initial="hidden"
-                        animate="visible"
-                        variants={navItemVariant}
+                        to={link.url}
+                        className={({ isActive }) =>
+                          twMerge(
+                            clsx(getBaseStyle(isDesktop, isHomePage), {
+                              "outline-white":
+                                isActive && isDesktop && isHomePage,
+                              "outline-black":
+                                isActive && isDesktop && !isHomePage,
+                              "text-red-500": isActive && !isDesktop,
+                            }),
+                          )
+                        }
                       >
-                        <NavLink
-                          key={index}
-                          to={link.url}
-                          className={({ isActive }) =>
-                            twMerge(
-                              clsx(getBaseStyle(isDesktop, isHomePage), {
-                                "outline-white":
-                                  isActive && isDesktop && isHomePage,
-                                "outline-black":
-                                  isActive && isDesktop && !isHomePage,
-                                "text-red-500": isActive && !isDesktop,
-                              }),
-                            )
-                          }
-                        >
-                          {link.title}
-                        </NavLink>
-                      </motion.div>
-                    );
-                  })}
-                </div>
-              </motion.div>
-            </AnimatePresence>
+                        {link.title}
+                      </NavLink>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
 
             {!isDesktop && (
               <Button
                 className="cursor-pointer"
                 onClick={() => setIsOpen(!isOpen)}
               >
-                <motion.div
+                <div
                   key={isOpen ? "close" : "menu"}
                   initial={{ rotate: 90, opacity: 0 }}
                   animate={{ rotate: 0, opacity: 1 }}
@@ -175,7 +145,7 @@ const Navbar = () => {
                   ) : (
                     <Menu size={24} className={iconColor} />
                   )}
-                </motion.div>
+                </div>
               </Button>
             )}
           </div>
